84c65fab08460e049443d1b9a9a7b48b74d5abd4
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2025-11-20 21:35:28 +0400

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Hakob Ghlijyan on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
